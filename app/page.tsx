88b793f63e91835c52b17f6c3f3c8d6dc546f664
amandeep <sardaramansingh.666@gmail.com>
2026-01-17 17:05:20 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center flex-col bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-8xl text-red-400">Wordcraft</h1>
+    </div>
+  );
+}
